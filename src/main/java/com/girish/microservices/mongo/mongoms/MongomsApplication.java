d71afebd52229084547f00932d08daaa3380cabf
Girish B <tuxinvincible@gmail.com>
2018-03-11 16:30:24 +0530

    Initial Version

---
+++ src/main/java/com/girish/microservices/mongo/mongoms/MongomsApplication.java
@@ -0,0 +1,12 @@
+package com.girish.microservices.mongo.mongoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongomsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/girish/microservices/mongo/mongoms/MongomsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.girish.microservices.mongo.mongoms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongomsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
