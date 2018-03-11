033011be42f23ddcc84657a1c6d877640a14be3c
Girish B <tuxinvincible@gmail.com>
2018-03-11 17:25:29 +0530

    Added functionality to add customer from webservice

--- src/main/java/com/girish/microservices/mongo/mongoms/MongoPersistenceController.java
+++ src/main/java/com/girish/microservices/mongo/mongoms/MongoPersistenceController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,12 @@ public class MongoPersistenceController {
 	@GetMapping("/customerByName")
 	public List<Customer> getCustomerByName(@RequestParam String firstName) {
 		return repository.findByFirstName(firstName);
+	}
+	
+	@PostMapping("customer")
+	public void saveCustomer(@RequestBody Customer customer)
+	{
+		repository.save(customer);
 	}
 
 }
