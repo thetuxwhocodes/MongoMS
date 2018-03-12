8b120140aadabf9207a7171b3967d0da5c2bb787
Girish B <tuxinvincible@gmail.com>
2018-03-12 18:29:25 +0530

    Added delete function

--- src/main/java/com/girish/microservices/mongo/mongoms/MongoPersistenceController.java
+++ src/main/java/com/girish/microservices/mongo/mongoms/MongoPersistenceController.java
@@ -3,7 +3,9 @@ package com.girish.microservices.mongo.mongoms;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,6 +45,13 @@ public class MongoPersistenceController {
 	public void saveCustomer(@RequestBody Customer customer)
 	{
 		repository.save(customer);
+	}
+
+	@DeleteMapping("/customer/{customerId}")
+	public void deleteCustomer(@PathVariable String customerId)
+	{
+		Customer customer = repository.findById(customerId).get();
+		repository.delete(customer);
 	}
 
 }
