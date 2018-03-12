59ad4eb1434330a74c5ea564f60fb46f6566f764
Girish B <tuxinvincible@gmail.com>
2018-03-12 18:39:35 +0530

    Split create and update to separate methods

--- src/main/java/com/girish/microservices/mongo/mongoms/MongoPersistenceController.java
+++ src/main/java/com/girish/microservices/mongo/mongoms/MongoPersistenceController.java
@@ -1,12 +1,14 @@
 package com.girish.microservices.mongo.mongoms;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,8 +44,15 @@ public class MongoPersistenceController {
 	}
 	
 	@PostMapping("/customer")
-	public void saveCustomer(@RequestBody Customer customer)
+	public void createCustomer(@RequestBody Customer customer)
 	{
+		repository.save(customer);
+	}
+
+	@PutMapping("/customer")
+	public void updateCustomer(@RequestBody Customer customer)
+	{
+		Objects.requireNonNull(customer.getId(), "Customer Id must be set for an Update operation.");
 		repository.save(customer);
 	}
 
